f289357f3dd7be9e964d4f53ea416dc8edf40f20
NiravShah1729 <nirav.s@ahduni.edu.in>
2025-06-14 23:53:37 +0530

    configure setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import e from 'express';
+import express from 'express';
 const app = express();
 
 app.use(cors({
@@ -14,4 +13,10 @@ app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
+
+//routes import 
+import userRouter from './routes/user.routes.js';
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
 export default app;
--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,20 @@
-import mongoose from 'mongoose';
-import { DB_NAME } from '../constants.js';
-
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-    try {
-        const connectionInstance  = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        process.exit(1); // Exit the process with failure
-    }
-}
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit the process with failure
+  }
+};
 
 export default connectDB;
